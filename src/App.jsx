f0f05fcd8750bafc254710b59ab7924860ef03b4
HeeJin <whgmlwls0219@naver.com>
2024-08-29 14:51:40 +0900

    add nav의 div생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+styled;
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="">빛나리</div>
+    </>
+  );
 }
 
 export default App;
