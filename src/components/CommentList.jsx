2345d8da3d4a2dfb59458dc999840f002e21a1f9
kyulipark <kyuli94p@gmail.com>
2024-09-03 19:48:30 +0900

    update: CommentList.jsx 댓글 라인 추가

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -3,8 +3,10 @@ import styled from "styled-components";
 const CommentList = ({ comments = [] }) => {
   return (
     <>
-      {comments.map((comment) => (
-        <StyledPostBox key={comment.id}>{comment.content}</StyledPostBox>
+      {comments.map((comment, index) => (
+        <StyledPostBox key={comment.id} isLast={index === comments.length - 1}>
+          {comment.content}
+        </StyledPostBox>
       ))}
     </>
   );
@@ -13,8 +15,23 @@ const CommentList = ({ comments = [] }) => {
 export default CommentList;
 
 const StyledPostBox = styled.div`
-  margin: 20px 0;
-  padding-bottom: 5px;
-  word-break: break-all;
-  text-align: start;
+  padding: 10px;
+  border-bottom: ${({ isLast }) => (isLast ? "none" : "1px solid #ddd")};
 `;
+
+// const CommentList = ({ comments = [] }) => {
+//   return (
+//     <>
+//       {comments.map((comment) => (
+//         <StyledPostBox key={comment.id}>{comment.content}</StyledPostBox>
+//       ))}
+//     </>
+//   );
+// };
+
+// const StyledPostBox = styled.div`
+//   margin: 20px 0;
+//   padding-bottom: 5px;
+//   word-break: break-all;
+//   text-align: start;
+// `;
